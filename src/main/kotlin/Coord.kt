c283ab7cc21a4355436210ca6cdedbbbcc8a69a6
Adam Kucera <adam.kucera@wrent.cz>
2019-12-17 08:58:36 +0100

    Solve 17th day

--- src/main/kotlin/Coord.kt
+++ src/main/kotlin/Coord.kt
@@ -6,4 +6,20 @@ data class Coord(val x: Int, val y: Int) {
     operator fun minus(decrement: Coord): Coord {
         return Coord(x - decrement.x, y - decrement.y)
     }
+
+    fun north(): Coord {
+        return Coord(x, y - 1)
+    }
+
+    fun south(): Coord {
+        return Coord(x, y + 1)
+    }
+
+    fun east(): Coord {
+        return Coord(x + 1, y)
+    }
+
+    fun west(): Coord {
+        return Coord(x - 1, y)
+    }
 }
