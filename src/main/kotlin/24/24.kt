a92d06e060db4b66379128a60bd285231c35558b
Adam Kucera <adam.kucera@wrent.cz>
2020-01-03 10:05:03 +0100

    Solve second part of day 24

--- src/main/kotlin/24/24.kt
+++ src/main/kotlin/24/24.kt
@@ -22,6 +22,118 @@ fun main() {
         nextMap = nextMap.mapValues { getNext(nextMap, it.key) }.toMutableMap()
     }
 
+    val recursiveMap = mutableMapOf<Int, MutableMap<Coord, Eris>>()
+    recursiveMap[0] = mutableMapOf()
+
+    INPUT24.split("\n")
+        .forEachIndexed { i, row ->
+            row.split("").filter { it != "" }.forEachIndexed { j, cell ->
+                val coord = Coord(j, i)
+                recursiveMap[0]!![coord] = eris(cell[0])
+            }
+        }
+
+    var nextRecursiveMap = recursiveMap
+    initLevels(nextRecursiveMap)
+    for (i in 0 until 200) {
+        val newMap = mutableMapOf<Int, MutableMap<Coord, Eris>>()
+        nextRecursiveMap.forEach {
+            newMap.putIfAbsent(it.key, mutableMapOf())
+            it.value.forEach { inner ->
+                val next = getNext(nextRecursiveMap, inner.key, it.key)
+                newMap[it.key]!![inner.key] = next
+            }
+        }
+        initLevels(newMap)
+        nextRecursiveMap = newMap
+    }
+    println("second result")
+    println(nextRecursiveMap.values.flatMap { it.values }.filter { it == Eris.BUG }.count())
+}
+
+fun initLevels(map: MutableMap<Int, MutableMap<Coord, Eris>>) {
+    initLevel(map, map.keys.min()!! - 1)
+    initLevel(map, map.keys.max()!! + 1)
+}
+
+private fun initLevel(map: MutableMap<Int, MutableMap<Coord, Eris>>, level: Int) {
+    map[level] = mutableMapOf()
+    for (i in 0..4) {
+        for (j in 0..4) {
+            map[level]!![Coord(i, j)] = Eris.EMPTY
+        }
+    }
+}
+
+fun getNext(map: MutableMap<Int, MutableMap<Coord, Eris>>, coord: Coord, level: Int): Eris {
+    if (coord == Coord(2, 2)) {
+        return Eris.EMPTY
+    }
+//    map.putIfAbsent(level - 1, mutableMapOf())
+    val count = (listOf(
+        map[level]!![coord.north()] ?: map[level - 1]?.get(Coord(2, 1)) ?: Eris.EMPTY,
+        map[level]!![coord.south()] ?: map[level - 1]?.get(Coord(2, 3)) ?: Eris.EMPTY,
+        map[level]!![coord.east()] ?: map[level - 1]?.get(Coord(3, 2)) ?: Eris.EMPTY,
+        map[level]!![coord.west()] ?: map[level - 1]?.get(Coord(1, 2)) ?: Eris.EMPTY
+    ) + addSubgrid(map, coord, level))
+        .filter { it == Eris.BUG }
+        .count()
+    when (map[level]!![coord]) {
+        Eris.BUG ->
+            return if (count == 1) {
+                Eris.BUG
+            } else {
+                Eris.EMPTY
+            }
+        Eris.EMPTY -> {
+            return if (count == 1 || count == 2) {
+                Eris.BUG
+            } else {
+                Eris.EMPTY
+            }
+        }
+    }
+    throw RuntimeException()
+}
+
+fun addSubgrid(map: MutableMap<Int, MutableMap<Coord, Eris>>, coord: Coord, level: Int): List<Eris> {
+//    map.putIfAbsent(level + 1, mutableMapOf())
+    return when (coord) {
+        Coord(2, 1) ->
+            listOf(
+                map[level + 1]?.get(Coord(0,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(1,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(2,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(3,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,0)) ?: Eris.EMPTY
+            )
+        Coord(2, 3) ->
+            listOf(
+                map[level + 1]?.get(Coord(0,4)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(1,4)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(2,4)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(3,4)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,4)) ?: Eris.EMPTY
+            )
+        Coord(1, 2) ->
+            listOf(
+                map[level + 1]?.get(Coord(0,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(0,1)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(0,2)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(0,3)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(0,4)) ?: Eris.EMPTY
+            )
+        Coord(3, 2) ->
+            listOf(
+                map[level + 1]?.get(Coord(4,0)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,1)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,2)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,3)) ?: Eris.EMPTY,
+                map[level + 1]?.get(Coord(4,4)) ?: Eris.EMPTY
+            )
+        else ->
+            listOf()
+    }
 }
 
 fun biodiversity(map: Map<Coord, Eris>): String {
